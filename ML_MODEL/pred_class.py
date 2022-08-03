cdbaea5744d7058cb6f77a1c9d35570ee2e6f08a
piyushkummaar <mapplecode2020@gmail.com>
2022-08-03 12:45:38 +0530

    changepath

--- ML_MODEL/pred_class.py
+++ ML_MODEL/pred_class.py
@@ -12,8 +12,8 @@ from sys import platform
 class prediction:
     def __init__(self):
         if platform == "linux" or platform == "linux2":
-            path = api.load('word2vec-google-news-300', return_path=True)
-            self.wv = gensim.models.KeyedVectors.load_word2vec_format(path, binary=True)
+            # path = api.load('word2vec-google-news-300', return_path=True)
+            self.wv = gensim.models.KeyedVectors.load_word2vec_format("/root/gensim-data/word2vec-google-news-300/word2vec-google-news-300.gz", binary=True)
         else:
             self.wv = gensim.models.KeyedVectors.load_word2vec_format("ML_MODEL/GoogleNews-vectors-negative300.bin.gz", binary=True)
         self.wv.init_sims(replace=True)
